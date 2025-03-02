531675a57e814c702f1d0804f472d7769178b57f
oosquare <oosquare@outlook.com>
2025-03-02 21:19:06 +0800

    init: initialize repository

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Rust library environment.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { self', inputs', system, ... }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
+        devShells.default = let
+          mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; };
+        in
+          mkShell {
+            buildInputs = [ rust ];
+            packages = with pkgs; [ nil ];
+          };
+      };
+    };
+}
