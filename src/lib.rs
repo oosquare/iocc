ed0b0a584d23ed772d063a635a608a840919e629
oosquare <oosquare@outlook.com>
2025-03-15 16:59:12 +0800

    feat: add the prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,11 @@ pub mod module;
 pub mod provider;
 pub mod scope;
 mod util;
+
+pub mod prelude {
+    pub use crate::container::injector::{InjectorError, TypedInjector};
+    pub use crate::container::registry::{Configurer, Registry, RegistryError};
+    pub use crate::container::Container;
+    pub use crate::module::{bind, Configuration, Module};
+    pub use crate::provider::Component;
+}
