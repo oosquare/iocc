2813ed2137547bcb340c10adec7fcaa8e47fb4b5
oosquare <oosquare@outlook.com>
2025-03-21 12:00:06 +0800

    style: format codes

--- src/iocc/src/lib.rs
+++ src/iocc/src/lib.rs
@@ -10,10 +10,10 @@ mod util;
 pub use iocc_derive::component;
 
 pub mod prelude {
+    pub use crate::component;
     pub use crate::container::injector::{InjectorError, TypedInjector};
     pub use crate::container::registry::{Configurer, Registry, RegistryError};
     pub use crate::container::Container;
     pub use crate::key;
     pub use crate::module::{bind, Configuration, Module};
-    pub use crate::component;
 }
